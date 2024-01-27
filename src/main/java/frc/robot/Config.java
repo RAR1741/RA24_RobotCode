c83553a032e2b273a982c4956ee8b8494ca00102
dracco1993 <dracco1993@gmail.com>
2024-01-27 15:25:43 -0500

    Formatted config
    Co-authored-by: Kryllyx <kryllyxofficial01@users.noreply.github.com>
    Co-authored-by: GingerJake <GingerJakeDaBoi@users.noreply.github.com>
    Co-authored-by: MePipe <NotMePipe@users.noreply.github.com>
    Co-authored-by: dominic-03 <dominic-03@users.noreply.github.com>
    Co-authored-by: HungryIronApple <HungryIronApple@users.noreply.github.com>

--- src/main/java/frc/robot/Config.java
+++ src/main/java/frc/robot/Config.java
@@ -12,82 +12,82 @@ import org.tomlj.TomlParseError;
 import org.tomlj.TomlParseResult;
 
 public class Config {
-    static TomlParseResult m_parsedConfig;
+  static TomlParseResult m_parsedConfig;
 
-    /**
-     * Load the configs from the specified file
-     *
-     * @param filepath Path to the config file
-     */
-    public static void loadFromFile(String filepath) {
-        try {
-            Path file = Paths.get(new File(filepath).getName());
+  /**
+   * Load the configs from the specified file
+   *
+   * @param filepath Path to the config file
+   */
+  public static void loadFromFile(String filepath) {
+    try {
+      Path file = Paths.get(new File(filepath).getName());
 
-            m_parsedConfig = Toml.parse(file);
-        } catch (IOException exception) {
-            System.err.printf("Unable to read from config file '%s':\n%s\n", filepath, exception.getMessage());
+      m_parsedConfig = Toml.parse(file);
+    } catch (IOException exception) {
+      System.err.printf("Unable to read from config file '%s':\n%s\n", filepath, exception.getMessage());
 
-            m_parsedConfig = null;
+      m_parsedConfig = null;
 
-            return;
-        }
-
-        if (!m_parsedConfig.errors().isEmpty()) {
-            System.err.printf("Error(s) parsing config file '%s':\n", filepath);
-
-            for (TomlParseError error : m_parsedConfig.errors()) {
-                System.err.println(error.getMessage());
-            }
-
-            m_parsedConfig = null;
-
-            return;
-        }
-
-        System.out.println("Config succesfully loaded...");
+      return;
     }
 
-    /**
-     * Get data assigned to the specified key
-     *
-     * @param key          The key
-     * @param defaultValue Value to be returned if no valid data is found
-     *
-     * @return The data found at the specified key
-     */
-    public static double getData(String key, double defaultValue) {
-        if (m_parsedConfig == null) {
+    if (!m_parsedConfig.errors().isEmpty()) {
+      System.err.printf("Error(s) parsing config file '%s':\n", filepath);
+
+      for (TomlParseError error : m_parsedConfig.errors()) {
+        System.err.println(error.getMessage());
+      }
+
+      m_parsedConfig = null;
+
+      return;
+    }
+
+    System.out.println("Config succesfully loaded...");
+  }
+
+  /**
+   * Get data assigned to the specified key
+   *
+   * @param key          The key
+   * @param defaultValue Value to be returned if no valid data is found
+   *
+   * @return The data found at the specified key
+   */
+  public static double getData(String key, double defaultValue) {
+    if (m_parsedConfig == null) {
+      return defaultValue;
+    }
+
+    return m_parsedConfig.getDouble(
+        key, new DoubleSupplier() {
+          @Override
+          public double getAsDouble() {
             return defaultValue;
-        }
+          }
+        });
+  }
 
-        return m_parsedConfig.getDouble(
-                key, new DoubleSupplier() {
-                    @Override
-                    public double getAsDouble() {
-                        return defaultValue;
-                    }
-                });
+  /**
+   * Get data assigned to the specified key
+   *
+   * @param key          The key
+   * @param defaultValue Value to be returned if no valid data is found
+   *
+   * @return The data found at the specified key
+   */
+  public static boolean getData(String key, boolean defaultValue) {
+    if (m_parsedConfig == null) {
+      return defaultValue;
     }
 
-    /**
-     * Get data assigned to the specified key
-     *
-     * @param key          The key
-     * @param defaultValue Value to be returned if no valid data is found
-     *
-     * @return The data found at the specified key
-     */
-    public static boolean getData(String key, boolean defaultValue) {
-        if (m_parsedConfig == null) {
+    return m_parsedConfig.getBoolean(
+        key, new BooleanSupplier() {
+          @Override
+          public boolean getAsBoolean() {
             return defaultValue;
-        }
-
-        return m_parsedConfig.getBoolean(
-                key, new BooleanSupplier() {
-                    @Override
-                    public boolean getAsBoolean() {
-                        return defaultValue;
-                    }
-                });
-    }
+          }
+        });
+  }
 }
