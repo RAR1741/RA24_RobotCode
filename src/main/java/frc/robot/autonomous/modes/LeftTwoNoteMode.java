b22a587463232234780404001429f39ca1585347
GingerJake <gingerjakey@outlook.com>
2024-03-28 18:27:50 -0400

    Update libraries

--- src/main/java/frc/robot/autonomous/modes/LeftTwoNoteMode.java
+++ src/main/java/frc/robot/autonomous/modes/LeftTwoNoteMode.java
@@ -15,30 +15,30 @@ import frc.robot.subsystems.Shooter.ShooterPivotTarget;
 import frc.robot.subsystems.Shooter.ShooterSpeedTarget;
 
 public class LeftTwoNoteMode extends AutoModeBase {
-    public void queueTasks() { // Set up left of subwoofer
+  public void queueTasks() { // Set up left of subwoofer
 
-        queueTask(new ShooterTask(ShooterPivotTarget.SUBWOOFER,
-                ShooterSpeedTarget.MAX));
-        queueTask(new IntakeTask(IntakePivotTarget.STOW, IntakeState.NONE));
+    queueTask(new ShooterTask(ShooterPivotTarget.SUBWOOFER,
+        ShooterSpeedTarget.MAX));
+    queueTask(new IntakeTask(IntakePivotTarget.STOW, IntakeState.NONE));
 
-        queueTask(new ParallelTask(
-                new AutoTargetTask(AllianceHelpers.getAllianceSpeakerPose3d()),
-                new WaitTask(ApolloConstants.Auto.Timing.k_shootRevTime)));
+    queueTask(new ParallelTask(
+        new AutoTargetTask(AllianceHelpers.getAllianceSpeakerPose3d()),
+        new WaitTask(ApolloConstants.Auto.Timing.k_shootRevTime)));
 
-        queueTask(new ParallelTask(
-                new IntakeTask(IntakePivotTarget.STOW, IntakeState.FEED_SHOOTER),
-                new WaitTask(ApolloConstants.Auto.Timing.k_shootFeedTime)));
+    queueTask(new ParallelTask(
+        new IntakeTask(IntakePivotTarget.STOW, IntakeState.FEED_SHOOTER),
+        new WaitTask(ApolloConstants.Auto.Timing.k_shootFeedTime)));
 
-        queueTask(new ParallelTask(
-                new IntakeTask(IntakePivotTarget.GROUND, IntakeState.INTAKE),
-                new DriveTrajectoryTask("Left, BotRing")));
+    queueTask(new ParallelTask(
+        new IntakeTask(IntakePivotTarget.GROUND, IntakeState.INTAKE),
+        new DriveTrajectoryTask("Left, BotRing")));
 
-        queueTask(new AutoTargetTask(AllianceHelpers.getAllianceSpeakerPose3d()));
+    queueTask(new AutoTargetTask(AllianceHelpers.getAllianceSpeakerPose3d()));
 
-        queueTask(new ParallelTask(
-                new IntakeTask(IntakePivotTarget.STOW, IntakeState.FEED_SHOOTER),
-                new WaitTask(ApolloConstants.Auto.Timing.k_shootFeedTime)));
+    queueTask(new ParallelTask(
+        new IntakeTask(IntakePivotTarget.STOW, IntakeState.FEED_SHOOTER),
+        new WaitTask(ApolloConstants.Auto.Timing.k_shootFeedTime)));
 
-        queueTask(new DriveForwardTask(0, 0));
-    }
+    queueTask(new DriveForwardTask(0, 0));
+  }
 }
