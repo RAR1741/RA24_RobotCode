8e1f6069c7450b0db3431e4d0638a3b277a9678c
GingerJake <gingerjakey@outlook.com>
2024-06-20 21:16:12 -0400

    Fix Apollo Auto

--- src/main/java/frc/robot/autonomous/tasks/DriveTrajectoryTask.java
+++ src/main/java/frc/robot/autonomous/tasks/DriveTrajectoryTask.java
@@ -26,7 +26,7 @@ import frc.robot.subsystems.drivetrain.RARHolonomicDriveController;
 import frc.robot.subsystems.drivetrain.SwerveDrive;
 
 public class DriveTrajectoryTask extends Task {
-  private final boolean k_debugPath = true;
+  private final boolean k_debugPath = false;
 
   private SwerveDrive m_swerve = SwerveDrive.getInstance();
   private PathPlannerTrajectory m_autoTrajectory;
@@ -84,7 +84,7 @@ public class DriveTrajectoryTask extends Task {
 
     // TODO: we probably want to do this all the time?
     if (!m_swerve.hasSetPose()) {
-      m_swerve.resetOdometry(getStartingPose());
+      // m_swerve.resetOdometry(getStartingPose()); //TODO: Use RobotConstants to figure out which robot we're using, Amadeus NEEDS this call.
     }
 
     m_swerve.clearTurnPIDAccumulation();
